3ee19e601dbc470c288bf4fb34b182913dd812f2
Vishnu vardhan.s <98823582+vishnuvardhancoder@users.noreply.github.com>
2024-07-27 19:43:10 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -19,8 +19,7 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('https://mern-auth-client-sigma.vercel.app
-/api/auth/signin', {
+      const res = await fetch('api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

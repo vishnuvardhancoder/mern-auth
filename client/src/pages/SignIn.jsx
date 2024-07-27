2aec422b180edc8f89ec432d42007f6c70f7ec47
Vishnu vardhan.s <98823582+vishnuvardhancoder@users.noreply.github.com>
2024-07-27 19:18:29 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -19,7 +19,8 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('mern-auth-five-snowy.vercel.app
+/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

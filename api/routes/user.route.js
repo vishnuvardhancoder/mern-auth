da14c8d6db32dde9a4c3d6a19a1f9835b58d6763
Vishnu vardhan.s <98823582+vishnuvardhancoder@users.noreply.github.com>
2024-07-27 19:15:14 +0530

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,9 +4,12 @@ import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
-router.get('/',test)
-router.post('/update/:id',verifyToken, updateUser)
-router.delete('/delete/:id',verifyToken, deleteUser)
+router.get('mern-auth-five-snowy.vercel.app
+/',test)
+router.post('mern-auth-five-snowy.vercel.app
+/update/:id',verifyToken, updateUser)
+router.delete('mern-auth-five-snowy.vercel.app
+/delete/:id',verifyToken, deleteUser)
 
 
 

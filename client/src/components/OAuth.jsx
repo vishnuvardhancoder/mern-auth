7140a16c6f4b202356be050db083a96b49808139
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-29 18:49:18 +0530

    updated oauth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -11,7 +11,7 @@ export default function OAuth() {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-
+      const result = await signInWithPopup(auth, provider);
       const res = await fetch('https://mern-auth-api-black.vercel.app/api/auth/google', {
         method: 'POST',
         headers: {

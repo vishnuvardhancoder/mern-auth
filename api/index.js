19a7c5e4258ff9bbe935a4db13b5677b6308c4b6
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-29 18:05:38 +0530

    removed cors to index.js

--- api/index.js
+++ api/index.js
@@ -1,48 +1,50 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Import cors
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+// import path from 'path'
+dotenv.config()
 
-dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("MongoDB connected Successfully!");
-}).catch((err) => {
-    console.log(err);
-});
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDB connected Successfully!")
+}).catch((err)=>{
+    console.log(err)
+})
 
-const app = express();
+// const __dirname = path.resolve()
 
-// CORS configuration
-app.use(cors({
-  origin: 'https://mern-auth-client-eight.vercel.app/', // Replace with your frontend's Vercel URL
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true
-}));
+const app = express()
 
-app.use(express.json());
-app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '/client/dist')))
+
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist','index.html' ))
+// })
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
 
-app.use((err, req, res, next) => {
+app.listen(3000, ()=>{
+    console.log("Server is running at port: 3000!!")
+})  
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
+app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+    const message = err.message || "Internal Server Error"
     return res.status(statusCode).json({
         success: false,
         message,
         statusCode,
-    });
-});
-
-app.listen(3000, () => {
-    console.log("Server is running at port: 3000!!");
-});
+    })
+})

640a599ed431e90d65da150db46181efaaacb88f
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-30 16:00:12 +0530

    added path

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,10 +25,12 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-    res
-      .cookie('access_token', token, { httpOnly: true, expires: expiryDate,domain: '.vercel.app' })
-      .status(200)
-      .json(rest);
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      expires: expiryDate,
+      domain: '.vercel.app',
+      path: '/'
+    })
   } catch (error) {
     next(error);
   }
@@ -41,12 +43,12 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: hashedPassword, ...rest } = user._doc;
       const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-      res
-        .cookie('access_token', token, {
-          httpOnly: true,
-          expires: expiryDate,
-          domain: '.vercel.app', 
-        })
+      res.cookie('access_token', token, {
+        httpOnly: true,
+        expires: expiryDate,
+        domain: '.vercel.app',
+        path: '/'
+      })
         .status(200)
         .json(rest);
     } else {
@@ -66,12 +68,12 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: hashedPassword2, ...rest } = newUser._doc;
       const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-      res
-        .cookie('access_token', token, {
-          httpOnly: true,
-          expires: expiryDate,
-          domain: '.vercel.app', 
-        })
+      res.cookie('access_token', token, {
+        httpOnly: true,
+        expires: expiryDate,
+        domain: '.vercel.app',
+        path: '/'
+      })
         .status(200)
         .json(rest);
     }

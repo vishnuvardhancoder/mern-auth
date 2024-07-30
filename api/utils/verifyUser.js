dbc33deb14f31191d2dea87d62a6a82bf2bda5dd
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-30 08:56:22 +0530

    modified auth and verify

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,7 +1,10 @@
 import  jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
+
 export const verifyToken = (req, res, next) => {
+    console.log('Verify token function called');
     const token = req.headers.cookie?.split('access_token=')[1];
+    console.log('Token from cookie:', token);
     if (!token) return next(errorHandler(401, 'You are not authenticated!'));
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) return next(errorHandler(401, 'Token is not valid'));

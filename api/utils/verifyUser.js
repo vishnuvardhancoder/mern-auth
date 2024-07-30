3ca31f60a787661fe9f677756b9f93ce0dcf710c
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-30 08:48:28 +0530

    added new verifyuser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,12 +1,11 @@
 import  jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
-export const verifyToken = (req,res,next) =>{
-    const token = req.cookies.access_token;
-
-    if (!token) return next(errorHandler(401,'You are not authenticated!'))
-    jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-        if(err) return next(errorHandler(401,"Token is not valid"))
-        req.user = user
-        next()
-    })
-}
+export const verifyToken = (req, res, next) => {
+    const token = req.headers.cookie?.split('access_token=')[1];
+    if (!token) return next(errorHandler(401, 'You are not authenticated!'));
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) return next(errorHandler(401, 'Token is not valid'));
+      req.user = user;
+      next();
+    });
+  };

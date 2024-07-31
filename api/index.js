09ed25edbf3bf15fb3e8bef0b436fd316527aaf1
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2024-07-31 16:37:10 +0530

    added import cors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 // import path from 'path';
 dotenv.config();
 
